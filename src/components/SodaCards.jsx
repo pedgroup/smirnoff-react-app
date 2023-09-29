e7c11e71aaceb83b16ef2d7acb165326370da279
jose Anton <jose.anton@pedestriangroup.com.au>
2023-09-29 12:40:28 +1000

    adding link to cards
    adding link to cards

--- src/components/SodaCards.jsx
+++ src/components/SodaCards.jsx
@@ -10,7 +10,9 @@ export const SodaCards = ({sodaCardsSuffle}) => {
         <Columns>
           {sodaCardsSuffle.map(activity => (
             <Column size='3' key={activity.title}>
-              <img width={'100%'} src={activity.image} alt={activity.title} />
+              <a href='https://urldefense.com/v3/__https:/shop-au-smirnoff.com/au/smirnoff/22501__;!!OK3MsjU!0Lk1PUX2mWwMlh0tRS_Vl9IIyg1p0zLTa3xrQWdMKLB21gvuDquD1t8a5f68oBXak-VvzYHEs-5Qb_Y_xe3hA_rvxQw$' target='_blank' rel='noreferrer'>
+                <img width={'100%'} src={activity.image} alt={activity.title} />
+              </a>
             </Column>
           ))}
         </Columns>
